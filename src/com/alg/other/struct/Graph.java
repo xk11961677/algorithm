956d9e8cd37dd65f37b20f12ccf6e5c519e9e139
shen11961677@163.com <shenliwei521>
2019-03-20 11:34:17 +0800

    Initial commit

---
+++ src/com/alg/other/struct/Graph.java
@@ -0,0 +1,46 @@
+package com.alg.other.struct;
+
+import java.util.LinkedList;
+
+/**
+ * 图数据结构(使用 [邻接表] 方式存储)
+ */
+public class Graph {
+
+    /**
+     * 定点个数vertex
+     */
+    private int v;
+
+
+    /**
+     * 邻接表
+     */
+    private LinkedList<Integer>[] adj;
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    /**
+     * 添加边 edge（无向图则需要添加两次）
+     */
+    public void addEdge(int s, int t) {
+        adj[s].add(t);
+        adj[t].add(s);
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public LinkedList<Integer>[] getAdj() {
+        return adj;
+    }
+
+
+}
